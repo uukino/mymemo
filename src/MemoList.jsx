bc18c344097ea53c1723a9df7f5397f818aa0584
chebuo <bob.chebuo@gmail.com>
2026-02-15 16:06:34 +0900

    [add] いいね数表示、背景色変更機能

--- src/MemoList.jsx
+++ src/MemoList.jsx
@@ -1,4 +1,4 @@
-function MemoList({ viewMode, currentList, formatTimestamp, handleEdit }) {
+function MemoList({ viewMode, currentList, formatTimestamp, handleEdit, pasteMemo, changeColor}) {
   return (
     <div>
       <hr />
@@ -24,12 +24,20 @@ function MemoList({ viewMode, currentList, formatTimestamp, handleEdit }) {
                   {memo.user_id ? `by ${memo.user_id}` : ""}
                 </span>
               ) : (
+                <div>
+                  <input type="color" value={memo.memoColor||"#fff8b0"} onChange={(e)=>{
+                    changeColor(memo,e);
+                  }}></input>
+                  <button onClick={()=>pasteMemo(memo)}>
+                    貼付
+                  </button>
                 <button
                   onClick={() => handleEdit(memo)}
                   style={{ fontSize: "12px", padding: "2px 8px" }}
                 >
                   編集
                 </button>
+                </div>
               )}
             </div>
           </li>
